af2ac102445d5811ac501d01a292e62d58ecc575
zhangyitao <>
2020-11-02 23:22:03 +0800

    list update

--- my/list/singly_list.py
+++ my/list/singly_list.py
@@ -18,52 +18,82 @@ class SList():
             else:
                 pre = q
                 q   = q.next
+        return None, None
 
     def find_node_and_pre_by_index(self, index):
-        if index > self.len:
-            return
+        if index == 0 or index > self.len:
+            print("not find_node_and_pre_by_index")
+            return None, None
         q   = self.head
         pre = self.head
-        while q:
-            if q.data == data:
-                return q, pre
-            else:
-                pre = q
-                q   = q.next
+        cur = 0
+        # 到下一个节点
+        while cur < index:
+            pre = q
+            q   = q.next
+            cur += 1
+        return q, pre
+
     # 后插
     def back_insert(self, data, index):
-        target, pre = self.find_by_index(index)
+        target, _ = self.find_node_and_pre_by_index(index)
         if not target:
             return
         new_node        = Node(data)
         new_node.next   = target.next
         target.next     = new_node
+        self.len        += 1
     # 前插
     def forward_insert(self, data, index):
-        target, pre = self.find_by_index(index)
+        target, pre = self.find_node_and_pre_by_index(index)
         if not target:
+            print("not")
             return
         new_node        = Node(data)
         new_node.next   = target
-        target.next     = new_node
+        pre.next        = new_node
+        self.len        += 1
     
     # 头插法
     def head_insert(self, data):
         new_node        = Node(data)
         new_node.next   = self.head.next   
         self.head.next  = new_node
+        self.len        += 1
     
     # 尾插法
-    def tail_insert():
+    def tail_insert(self):
         pass
 
     def delete_node(self, data, index):
-        target, pre = self.find_by_index(index)
+        target, pre = self.find_node_and_pre_by_index(index)
         if not target:
             return
         pre.next    = target.next
         target.next = None
         target.data = None
+        self.len    -= 1
 
-if __name__ == 'main':
-    pass
+    def __repr__(self):
+        t = []
+        q = self.head.next
+        while q:
+            t.append(str(q.data))
+            q = q.next
+        return '->'.join(t)
+            
+
+if __name__ == "__main__":
+    sl = SList()
+    sl.head_insert(1)
+    sl.head_insert(2)
+    sl.head_insert(3)
+    sl.head_insert(4)
+    print(sl)
+
+    sl.forward_insert(1111, 0)
+    print(sl)
+    sl.forward_insert(1111, 1)
+    print(sl)
+    sl.back_insert(1112, 2)
+    print(sl)
